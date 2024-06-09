61243ccde8a38ed453e548963a5a670616bde3f2
eaadalen <eaadalen112@gmail.com>
2024-06-08 22:12:40 -0500

    Change short prompt endpoint
    Return one large JSON object instead of requiring client to make several requests

--- index.js
+++ index.js
@@ -60,19 +60,25 @@ app.get('/random', (req, res) => {
       });
 });
 
-// Get a short prompt based on the letter of the selected long prompt
-app.get('/spL/:letter', (req, res) => {
-  shortPrompts.aggregate([
-    { $match: { Answer : { $regex : req.params.letter } } },
-    { $sample: { size: 1 } }
-  ])
+// Get a short prompt based on the letters of the selected long prompt
+app.get('/spL/:letters', (req, res) => {
+  let promptResponse = {}
+
+  Array.from(req.params.letters).forEach((element) => {
+    shortPrompts.aggregate([
+      { $match: { Answer : { $regex : req.params.letters } } },
+      { $sample: { size: 1 } }
+    ])
     .then((prompt) => {
-      res.status(201).json(prompt);
+      promptResponse[element] = prompt
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
+  })
+
+  res.status(201).json(promptResponse);
 });
 
 // Get full list of short prompts
