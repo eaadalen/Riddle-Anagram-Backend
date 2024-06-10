78af11a8a516308771b569045166590f9794a942
eaadalen <eaadalen112@gmail.com>
2024-06-10 13:00:40 -0500

    TS

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/spL/:letters', (req, res) => {
   let shuffle = shuffleString(req.params.letters)
   Array.from(shuffle).forEach((element) => {
     shortPrompts.aggregate([
-      { $match: { $and : [{ Answer : { $regex : element }},  { shortPrompt : 'GREEN' }]}},
+      { $match: { $and : [{ Answer : { $regex : element }},  { shortPrompt : 'Red-_____ colorblind' }]}},
       { $sample: { size: 1 } }
     ])
     .then((prompt) => {
